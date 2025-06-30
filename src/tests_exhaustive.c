bc0ef8fd0b5ff72a4758f87ec0c0ddcf5ca9f00f
Manny27nyc <Fordamboy1@gmail.com>
2025-06-30 16:41:12 -0400

    🔒 Inject minimal licensing header (automated)

--- src/tests_exhaustive.c
+++ src/tests_exhaustive.c
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 /*
  * Copyright (c) 2008–2025 Manuel J. Nieves (a.k.a. Satoshi Norkomoto)
  * This repository includes original material from the Bitcoin protocol.
